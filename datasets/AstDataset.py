fix KL vanishing issue

datasets/AstDataset.py
@@ -123,7 +123,7 @@ class AstDataset(IterableDataset):
         return nodes, depths
 
     def get_amt_nodes(self, root, nodes=0, iteration = 1):
-        if 'children' in root and iteration < 2:
+        if 'children' in root: #and iteration < 2:
             to_remove = []
             for child in root['children']:
                 # if child['res'] and not 'children' in child:
@@ -143,14 +143,14 @@ class AstDataset(IterableDataset):
         return nodes + 1
 
     def get_max_depth(self, root, iteration = 1):
-        if 'children' in root and iteration < 2:
+        if 'children' in root: # and iteration < 2:
             return 1 + max(self.get_max_depth(child, iteration + 1) for child in root['children'])
         else:
             return 1
 
     def _label_node_index(self, node, n=0, iteration = 1):
         node['index'] = n
-        if 'children' in node and iteration < 2:
+        if 'children' in node: # and iteration < 2:
             for child in node['children']:
                 n += 1
                 n = self._label_node_index(child, n, iteration + 1)
@@ -194,19 +194,18 @@ class AstDataset(IterableDataset):
 
         features = [[torch.tensor([feature])]]
         vocabs = [vocab]
-        if iteration < 2:
-            if 'children' in node:
-                for child in node['children']:
-                    feature, vocab, nameid_to_placeholderid = self._gather_node_attributes(
-                        child, key, node, nameid_to_placeholderid, iteration + 1)
-                    features.extend(feature)
-                    vocabs += vocab
+        if 'children' in node: # and iteration < 2:
+            for child in node['children']:
+                feature, vocab, nameid_to_placeholderid = self._gather_node_attributes(
+                    child, key, node, nameid_to_placeholderid, iteration + 1)
+                features.extend(feature)
+                vocabs += vocab
 
         return features, vocabs, nameid_to_placeholderid
 
     def _gather_adjacency_list(self, node, iteration = 1):
         adjacency_list = []
-        if 'children' in node and iteration < 2:
+        if 'children' in node: # and iteration < 2:
             for child in node['children']:
                 adjacency_list.append([node['index'], child['index']])
                 adjacency_list.extend(self._gather_adjacency_list(child, iteration + 1))

models/TreeLstmDecoderComplete.py
@@ -316,8 +316,9 @@ class TreeLstmDecoderComplete(nn.Module):
                                                                         # + self.offset_sibling(has_sibling[vocabs_mask == k])
                                                                         ), dim=-1) == label[tree_state.vocabs_mask == k].view(-1)).item()
                             
-                            pred_labels = torch.argmax(self.softmax(label_pred), dim=-1)
-                            print(iteration, sibling_index, pred_labels[pred_labels != label[tree_state.vocabs_mask == k].view(-1)].tolist(), pred_labels.tolist(), label[tree_state.vocabs_mask == k].view(-1).tolist()) #, self.softmax(label_pred).topk(3, sorted=True)[1], self.softmax(label_pred).topk(3, sorted=True)[0], h_pred[:, :5],)
+                            # print(h_pred[tree_state.vocabs_mask == k], parent_state, sibling_state)
+                            # pred_labels = torch.argmax(self.softmax(label_pred), dim=-1)
+                            # print(iteration, sibling_index, pred_labels[pred_labels != label[tree_state.vocabs_mask == k].view(-1)].tolist(), pred_labels.tolist(), label[tree_state.vocabs_mask == k].view(-1).tolist()) #, self.softmax(label_pred).topk(3, sorted=True)[1], self.softmax(label_pred).topk(3, sorted=True)[0], h_pred[:, :5],)
                         loss += label_loss #* max(-100 * iteration + 200, 1)
 
                         individual_losses[k] += label_loss.item()
@@ -393,6 +394,8 @@ class TreeLstmDecoderComplete(nn.Module):
                                     # Update the hidden and cell values matrices
                                     tree_state.h_s[i][tree_state.current_nodes_indices[tree_state.vocabs_mask == k]] = h_sibling_new
                                     tree_state.c_s[i][tree_state.current_nodes_indices[tree_state.vocabs_mask == k]] = c_sibling_new
+
+                # break
 
         return loss
 

models/TreeLstmEncoderComplete.py
@@ -46,14 +46,15 @@ class TreeLstmEncoderComplete(nn.Module):
         adj_list = inp['adjacency_list']
         edge_order = inp['edge_order_bottomup']
         vocabs = inp['vocabs']
+        total_nodes = node_order.shape[0]
 
-        features = torch.zeros(node_order.shape[0], self.params['EMBEDDING_DIM'], device=self.device)
+        features = torch.zeros(total_nodes, self.params['EMBEDDING_DIM'], device=self.device)
         h = []
         c = []
 
         for _ in range(self.params['NUM_LSTM_LAYERS']):
-            h.append(torch.zeros(node_order.shape[0], self.hidden_size, device=self.device))
-            c.append(torch.zeros(node_order.shape[0], self.hidden_size, device=self.device))
+            h.append(torch.zeros(total_nodes, self.hidden_size, device=self.device))
+            c.append(torch.zeros(total_nodes, self.hidden_size, device=self.device))
         
         for k, embedding_layer in self.embedding_layers.items():
             if self.params['INDIV_LAYERS_VOCABS']:
@@ -106,7 +107,7 @@ class TreeLstmEncoderComplete(nn.Module):
         # tree_states = torch.cat([hidden, cell], dim=-1)
 
         for idx, tree_state in enumerate(torch.split(hidden, inp['tree_sizes'])):
-            hidden_roots[i] = torch.max(tree_state, dim=0)[0]
+            hidden_roots[idx] = torch.max(tree_state, dim=0)[0]
 
         # Get z_mean and z_log_var from hidden (parent roots only)
         z_mean = self.z_mean(hidden_roots)
@@ -117,10 +118,11 @@ class TreeLstmEncoderComplete(nn.Module):
         z = self.reparameterize(z_mean, z_log_var)
 
         if self.vae:
-            kl_loss = 0.5 * torch.sum(z_log_var.exp() - z_log_var - 1 + z_mean.pow(2))
+            kl_loss = (0.5 * torch.sum(z_log_var.exp() - z_log_var - 1 + z_mean.pow(2)))
+            # Make sure KL divergence is scaled the same as the reconstruction loss
+            kl_loss /= total_nodes
         else:
-            kl_loss = torch.tensor([0], device=self.device)
-
+            kl_loss = torch.tensor([0], device=self.device)        
         
         # Return latent vector, and mean and variance
         return z, kl_loss

models/Vae.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 from tqdm import tqdm
-# from tqdm import tqdm_notebook as tqdm
+import math
 import torch
 from torch import optim
 import torch.nn as nn
@@ -88,7 +88,11 @@ class Vae(nn.Module):
 
         self.train()
 
-        kl_scheduler = KLScheduling.CyclicalAnnealing(287*epochs, 300)
+        iterations = math.ceil((self.params['TOKEN_VOCABS']['RES']['root'] / self.params['BATCH_SIZE']) * epochs)
+
+        print(iterations)
+
+        kl_scheduler = KLScheduling.CyclicalAnnealing(iterations, nr_warmup_iterations=300)
 
         running_losses = {}
         loss_types_train = list(self.embedding_layers.keys()) + \