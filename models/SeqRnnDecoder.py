fix KL vanishing issue seq2seq model, TODO: still vanishing with LSTM or RNN, only works with GRU

models/SeqRnnDecoder.py
@@ -1,5 +1,4 @@
 import torch
-from torch._C import device
 import torch.nn as nn
 import torch.nn.utils.rnn as rnn_utils
 from model_utils.adaptive_softmax_pytorch import AdaptiveLogSoftmaxWithLoss
@@ -19,51 +18,57 @@ class SeqRnnDecoder(nn.Module):
                 latent_dim,
                 num_rnn_layers_dec,
                 recurrent_dropout,
-                bidirectional=False,
-                max_sequence_length=1500):
+                max_program_size,
+                word_dropout=0.25,
+                rnn_type='lstm',
+                ):
 
         super().__init__()
 
         self.device = device
         self.latent_dim = latent_dim
-        self.bidirectional = bidirectional
         self.num_rnn_layers = num_rnn_layers_dec
         self.rnn_hidden_size = rnn_hidden_size
-        self.max_sequence_length = max_sequence_length
+        self.max_program_size = max_program_size
         self.vocab_size = vocabulary.get_vocab_size('ALL')
+        self.rnn_type = rnn_type
+        self.word_dropout = word_dropout # replace percentage of tokens with unk token to make model depend more on latent variable
         print(self.vocab_size)
 
         self.embedding_layer = embedding_layers['ALL']
 
-        self.rnn = nn.RNN(embedding_dim,
+        if rnn_type == 'rnn':
+            rnn = nn.RNN
+        elif rnn_type == 'gru':
+            rnn = nn.GRU
+        elif rnn_type == 'lstm':
+            rnn = nn.LSTM
+
+        self.rnn = rnn(embedding_dim,
                           rnn_hidden_size,
                           num_layers=num_rnn_layers_dec, 
-                          bidirectional=bidirectional, 
                           batch_first=True, 
                           dropout=recurrent_dropout)
 
-        self.hidden_factor = (2 if bidirectional else 1) * num_rnn_layers_dec
+        self.hidden_factor = num_rnn_layers_dec
 
         self.latent2hidden = nn.Linear(latent_dim, rnn_hidden_size * self.hidden_factor)
 
 
         # Adaptive softmax loss does not need prediction layer, much faster approach
         # for calculating softmax with highly imbalanced vocabs
-        # cutoffs: 30: 70.1%, 31-100: 18.0%, 101-1000: 9.3%, rest: 2.6%
+        # cutoffs: 100: 90.2%, 101-1000: 7.9%, 1001-10000: 1.3%, rest: 0.4%
         self.loss = AdaptiveLogSoftmaxWithLoss(
                                             self.rnn_hidden_size,
                                             self.vocab_size,
-                                            cutoffs=[30, 100, 1000],
-                                            div_value=4.0)
-
-        # self.prediction_layer = nn.Linear(rnn_hidden_size * (2 if bidirectional else 1), self.vocab_size)
-
-        # self.loss = nn.CrossEntropyLoss(reduction='sum')
+                                            cutoffs=[100, 1000, 10000],
+                                            div_value=2.0)
 
 
         self.sos_idx = vocabulary.token2index['ALL']['<sos>']
         self.eos_idx = vocabulary.token2index['ALL']['<eos>']
         self.pad_idx = vocabulary.token2index['ALL']['<pad>']
+        self.unk_idx = vocabulary.token2index['ALL']['<unk>']
 
 
     def forward(self, z, inp=None, temperature=None, top_k=None, top_p=None):
@@ -84,11 +89,23 @@ class SeqRnnDecoder(nn.Module):
 
         hidden = self.latent2hidden(z)
 
-        if self.bidirectional or self.num_rnn_layers > 1:
+        if self.num_rnn_layers > 1:
             # Unflatten the hidden state
             hidden = hidden.view(self.hidden_factor, batch_size, self.rnn_hidden_size)
         else:
             hidden = hidden.unsqueeze(0)
+
+        if self.rnn_type == 'lstm':
+            cell = torch.zeros((self.hidden_factor, batch_size, self.rnn_hidden_size), device=self.device)
+            hidden = (hidden, cell)
+
+
+        if self.word_dropout > 0 and self.training:
+            # randomly replace decoder input with <unk>
+            prob = torch.rand(inp_seq.size(), device=self.device)
+            prob[(inp_seq.data - self.sos_idx) * (inp_seq.data - self.pad_idx) == 0] = 1
+            inp_seq[prob < self.word_dropout] = self.unk_idx
+
 
         inp_emb = self.embedding_layer(inp_seq)
 
@@ -103,18 +120,13 @@ class SeqRnnDecoder(nn.Module):
         padded_outputs = padded_outputs[reversed_idx]
         _,s,_ = padded_outputs.size()
 
-
-        # Get predictions
-        # logits = self.prediction_layer(padded_outputs.view(-1, padded_outputs.size(2)))
-        # loss = self.loss(logits)
-
         # cut-off unnessary padding from target and flatten
         target_seq = target_seq[:, :s].contiguous().view(-1)
 
         # "flatten" output as well to (B * max_seq_len, hidden_size)
         padded_outputs = padded_outputs.view(-1, self.rnn_hidden_size)
 
-        _, loss = self.loss(padded_outputs, target_seq)
+        outputs, loss = self.loss(padded_outputs, target_seq)
 
         # We do not want to punish the model for predicting padding incorrectly
         # Therefore we set the loss for the paddings to 0
@@ -131,11 +143,15 @@ class SeqRnnDecoder(nn.Module):
 
         hidden = self.latent2hidden(z)
 
-        if self.bidirectional or self.num_rnn_layers > 1:
+        if self.num_rnn_layers > 1:
             # Unflatten the hidden state
             hidden = hidden.view(self.hidden_factor, batch_size, self.rnn_hidden_size)
         else:
             hidden = hidden.unsqueeze(0)
+
+        if self.rnn_type == 'lstm':
+            cell = torch.zeros((self.hidden_factor, batch_size, self.rnn_hidden_size), device=self.device)
+            hidden = (hidden, cell)
 
         
          # required for dynamic stopping of sentence generation
@@ -146,10 +162,10 @@ class SeqRnnDecoder(nn.Module):
         # idx of still generating sequences with respect to current loop
         running_seqs = torch.arange(0, batch_size, device=self.device, dtype=torch.long)
 
-        generations = torch.full(fill_value=self.pad_idx, size=(batch_size, self.max_sequence_length), device=self.device, dtype=torch.long)
+        generations = torch.full(fill_value=self.pad_idx, size=(batch_size, self.max_program_size), device=self.device, dtype=torch.long)
 
         t = 0
-        while t < self.max_sequence_length and len(running_seqs) > 0:
+        while t < self.max_program_size and len(running_seqs) > 0:
 
             if t == 0:
                 sequence = torch.full(size=(batch_size,), fill_value=self.sos_idx, dtype=torch.long, device=self.device)
@@ -168,7 +184,7 @@ class SeqRnnDecoder(nn.Module):
             generations = self._save_sample(generations, sequence, sequence_running, t)
 
 
-            # update gloabl running sequence
+            # update global running sequence
             sequence_mask[sequence_running] = (sequence != self.eos_idx)
             sequence_running = sequence_idx.masked_select(sequence_mask)
 
@@ -179,7 +195,11 @@ class SeqRnnDecoder(nn.Module):
             # prune input and hidden state according to local update
             if len(running_seqs) > 0:
                 sequence = sequence[running_seqs]
-                hidden = hidden[:, running_seqs]
+
+                if self.rnn_type == 'lstm':
+                    hidden = (hidden[0][:, running_seqs], hidden[1][:, running_seqs])
+                else:
+                    hidden = hidden[:, running_seqs]
 
                 running_seqs = torch.arange(0, len(running_seqs), device=self.device, dtype=torch.long)
 

models/SeqRnnEncoder.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.nn.utils.rnn as rnn_utils
 from config.vae_config import ex
+from model_utils.modules import Highway
+
 
 
 """
@@ -21,27 +24,43 @@ class SeqRnnEncoder(nn.Module):
                 vae,
                 num_rnn_layers_enc,
                 recurrent_dropout,
-                bidirectional=False):
+                max_program_size,
+                rnn_type='lstm',
+                rnn_encoder_bidirectional=False):
 
         super().__init__()
 
         self.device = device
         self.latent_dim = latent_dim
         self.vae = vae
-        self.bidirectional = bidirectional
+        self.bidirectional = rnn_encoder_bidirectional
         self.num_rnn_layers = num_rnn_layers_enc
         self.rnn_hidden_size = rnn_hidden_size
+        self.rnn_type = rnn_type
+        self.embedding_dim = embedding_dim
+        self.max_program_size = max_program_size
 
         self.embedding_layer = embedding_layers['ALL']
 
-        self.rnn = nn.RNN(embedding_dim,
+
+        # self.highway = Highway(embedding_dim, 2, F.relu)
+
+
+        if rnn_type == 'rnn':
+            rnn = nn.RNN
+        elif rnn_type == 'gru':
+            rnn = nn.GRU
+        elif rnn_type == 'lstm':
+            rnn = nn.LSTM
+
+        self.rnn = rnn(embedding_dim,
                           rnn_hidden_size,
                           num_layers=num_rnn_layers_enc, 
-                          bidirectional=bidirectional, 
+                          bidirectional=self.bidirectional, 
                           batch_first=True, 
                           dropout=recurrent_dropout)
 
-        self.hidden_factor = (2 if bidirectional else 1) * num_rnn_layers_enc
+        self.hidden_factor = (2 if self.bidirectional else 1) * num_rnn_layers_enc
 
         self.z_mean = nn.Linear(rnn_hidden_size * self.hidden_factor, latent_dim)
         self.z_log_var = nn.Linear(rnn_hidden_size * self.hidden_factor, latent_dim)
@@ -55,10 +74,19 @@ class SeqRnnEncoder(nn.Module):
         sorted_lengths, sorted_idx = torch.sort(length, descending=True)
         inp_seq = inp_seq[sorted_idx]
 
+
         inp_emb = self.embedding_layer(inp_seq)
-        packed_inp = rnn_utils.pack_padded_sequence(inp_emb, sorted_lengths.data.tolist(), batch_first=True)
+
+        # inp_highway = inp_emb.view(-1, self.embedding_dim)
+        # inp_rnn = self.highway(inp_highway)
+        inp_rnn = inp_emb.view(batch_size, self.max_program_size, self.embedding_dim)
+
+        packed_inp = rnn_utils.pack_padded_sequence(inp_rnn, sorted_lengths.data.tolist(), batch_first=True)
 
         _, hidden = self.rnn(packed_inp)
+        
+        if self.rnn_type == 'lstm':
+            hidden = hidden[0]
 
         if self.bidirectional or self.num_rnn_layers > 1:
             # Flatten the hidden state
@@ -67,13 +95,13 @@ class SeqRnnEncoder(nn.Module):
             hidden = hidden.squeeze()
 
 
-        # Get z_mean and z_log_var from hidden (parent roots only)
+        # Get z_mean and z_log_var from hidden
         z_mean = self.z_mean(hidden)
         z_log_var = self.z_log_var(hidden)
-
         
         # Parameterization trick
         z = self.reparameterize(z_mean, z_log_var)
+
 
 
         if self.vae:
@@ -88,6 +116,7 @@ class SeqRnnEncoder(nn.Module):
     def reparameterize(self, mu, log_var):
         if self.training and self.vae:
             std = torch.exp(0.5 * log_var)
+
             eps = std.data.new(std.size()).normal_()
 
             return eps.mul(std).add_(mu)

models/Vae.py
@@ -28,7 +28,6 @@ class Vae(nn.Module):
 
         self.encoder = encoder(device, self.embedding_layers)
         self.decoder = decoder(device, self.embedding_layers, vocabulary, loss_weights)
-        # self.encoder = TreeSeqRnnEncoder(device, params, self.embedding_layers).to(device)
 
         # Store losses -> so we can easily save them
         self.metrics = {}
@@ -126,7 +125,7 @@ class Vae(nn.Module):
 
             kl_loss, reconstruction_loss, individual_losses, accuracies = self(batch)
 
-            loss = kl_loss * kl_weight + reconstruction_loss
+            loss = (kl_loss * kl_weight + reconstruction_loss) / len(batch['input'])
 
             loss.backward()
 
@@ -246,11 +245,6 @@ class Vae(nn.Module):
 
                 # if iterations > 0:
                 #     break
-
-
-        # Get the average of the bleu scores over the entire test dataset
-        # for key in avg_tree_bleu_scores.keys():
-        #     avg_tree_bleu_scores[key] /= iterations
 
         bleu_scores = evaluator.calc_bleu_score()
 

test_seq2seq.py
@@ -17,7 +17,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @ex.config
 def set_config():
-    pretrained_model = '../vastai/168_150latent_seq2seq/model.tar'
+    pretrained_model = 'checkpoints/1084/iter1000.tar' #'../vastai/168_150latent_seq2seq/model.tar'
 
     folder = os.path.dirname(pretrained_model)
     
@@ -27,13 +27,12 @@ def set_config():
 
 
     # Overwrite config pretrained model
-    ex.add_config({'pretrained_model': pretrained_model, 'batch_size': 32})
+    ex.add_config({'pretrained_model': pretrained_model, 'batch_size': 2})
 
 
 class Tester:
     def __init__(self):
         self.vocabulary = self.get_vocabulary()
-        self.add_special_vocab_tokens()
         self.loss_weights = self.get_loss_weights()
         self.model = self.make_model()
         self.load_model()
@@ -69,7 +68,7 @@ class Tester:
 
         tokens_paths = {'ALL': tokens_paths['ALL']}
 
-        vocabulary = Vocabulary(tokens_paths, max_tokens)
+        vocabulary = Vocabulary(tokens_paths, max_tokens, add_special_tokens=True)
 
         return vocabulary        
 
@@ -91,8 +90,6 @@ class Tester:
     def get_datasets(self, dataset_paths, max_program_size):
         test_files = [os.path.join(dataset_paths['TEST'], file) for file in os.listdir(dataset_paths['TEST'])]
 
-        print(test_files)
-
         test_datasets = list(map(lambda x : SeqDataset(x, self.vocabulary, max_program_size, device), test_files))
         test_dataset = ConcatDataset(test_datasets)
 
@@ -108,14 +105,6 @@ class Tester:
 
         
         return test_loader
-
-
-    def add_special_vocab_tokens(self):
-        self.vocabulary.add_new_token('ALL', '<pad>')
-        self.vocabulary.add_new_token('ALL', '<unk>')
-        self.vocabulary.add_new_token('ALL', '<sos>')
-        self.vocabulary.add_new_token('ALL', '<eos>')
-
 
 
 

train_seq2seq.py
@@ -11,7 +11,6 @@ import json
 from model_utils.vocabulary import Vocabulary
 from torch import optim
 from config.vae_config import ex
-from random import sample
 from models.SeqRnnEncoder import SeqRnnEncoder
 from models.SeqRnnDecoder import SeqRnnDecoder
 from model_utils.metrics_helper import MetricsHelperSeq2Seq
@@ -24,16 +23,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class Trainer:
     def __init__(self):
         self.vocabulary = self.get_vocabulary()
-        self.add_special_vocab_tokens()
         self.loss_weights = self.get_loss_weights()
-        print('making model')
         self.model = self.make_model()
         self.set_optimizer()
         self.load_model()
-        print('making datasets')
         self.train_dataset, self.val_dataset, self.test_dataset = self.get_datasets()
         self.train_loader, self.val_loader, self.test_loader = self.get_dataloaders()
-        print('getting scheduler')
         self.kl_scheduler = self.get_kl_scheduler()
 
 
@@ -61,7 +56,7 @@ class Trainer:
             'ALL': None
         }
 
-        vocabulary = Vocabulary(tokens_paths, max_tokens)
+        vocabulary = Vocabulary(tokens_paths, max_tokens, add_special_tokens=True)
 
         return vocabulary        
 
@@ -81,10 +76,6 @@ class Trainer:
 
     @ex.capture
     def get_datasets(self, dataset_paths, max_program_size):
-        # files = set(os.path.join(dataset_path, file) for file in os.listdir(dataset_path) if 'programs' in file)
-
-        # train_files, val_files, test_files = self.create_train_val_test_split(files)
-
         train_files = [os.path.join(dataset_paths['TRAIN'], file) for file in os.listdir(dataset_paths['TRAIN'])]
         val_files = [os.path.join(dataset_paths['VAL'], file) for file in os.listdir(dataset_paths['VAL'])]
         test_files = [os.path.join(dataset_paths['TEST'], file) for file in os.listdir(dataset_paths['TEST'])]
@@ -100,19 +91,7 @@ class Trainer:
 
         self.num_train_programs = sum([len(dset) for dset in train_datasets])
 
-        return train_dataset, val_dataset, test_dataset
-
-
-    def create_train_val_test_split(self, files, val_test_files=13):
-        test_files = sample(files, val_test_files)
-        
-        files = files - set(test_files)
-
-        val_files = sample(files, val_test_files)
-
-        train_files = files - set(val_files)
-
-        return train_files, val_files, test_files
+        return train_dataset, val_dataset, test_dataset   
 
 
     @ex.capture
@@ -151,14 +130,6 @@ class Trainer:
         elif kl_scheduling == 'cyclical':
             iterations = math.ceil((self.num_train_programs / batch_size) * num_epochs)
             return KLScheduling.CyclicalAnnealing(iterations, kl_warmup_iters, kl_cycles, kl_ratio, kl_function)
-
-
-    def add_special_vocab_tokens(self):
-        self.vocabulary.add_new_token('ALL', '<pad>')
-        self.vocabulary.add_new_token('ALL', '<unk>')
-        self.vocabulary.add_new_token('ALL', '<sos>')
-        self.vocabulary.add_new_token('ALL', '<eos>')
-
 
 
 